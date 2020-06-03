eff54b011cfae988fd97e129622e8421a9969f24
toms0 <58892488+toms0@users.noreply.github.com>
2020-06-03 18:28:14 +0100

    Ready for deployment to Heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -129,3 +129,12 @@ STATIC_URL = '/static/'
 
 # My Settings.
 LOGIN_URL = 'users:login'
+
+# Heroku Settings.
+import django_heroku
+django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
